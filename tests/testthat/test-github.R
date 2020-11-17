3f6c03a92c2bb0bfe05065691473c9c2406d3729
Maximilian Held <info@maxheld.de>
2020-11-17 22:20:18 +0100

    fix local failed test

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -1,5 +1,10 @@
 test_that("github packages image url is correct", {
-  withr::local_envvar(new = c("MUGGLE_PKG_NAME" = "muggle"))
+  # outside of muggle container, this does not exist
+  # but on ghactions, it should exist
+  # TODO should actually test whether it's inside muggle container
+  if (!is_github_actions()) {
+    withr::local_envvar(.new = c("MUGGLE_PKG_NAME" = "muggle"))
+  }
   expect_equal(
     gh_pkgs_image_url(version = "latest", repo_spec = "subugoe/muggle"),
     "docker.pkg.github.com/subugoe/muggle/muggle-runtime:latest"
