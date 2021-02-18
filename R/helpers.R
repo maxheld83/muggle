065cf067d9677a94b69b1f01ae60756a37ab37d6
Maximilian Held <info@maxheld.de>
2021-02-18 18:07:08 +0100

    add is github actions helper

--- R/helpers.R
+++ R/helpers.R
@@ -66,3 +66,12 @@ local_muggle_file <- function(muggle_file, .local_envir = parent.frame()) {
     fs::file_copy(path = get_muggle_file(muggle_file), new_path = target)
   }
 }
+
+#' Determine if code is running inside GitHub Actions
+#'
+#' Looks for the `GITHUB_ACTIONS` environment variable, as [documented](https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables)
+#'
+#' @keywords internal
+is_github_actions <- function() {
+  Sys.getenv("GITHUB_ACTIONS") == "true"
+}
